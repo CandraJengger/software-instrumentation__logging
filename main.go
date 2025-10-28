781d16c450783ef415866e2265b67def6acf5399
CandraJengger <cakjengger@gmail.com>
2025-10-29 06:43:30 +0700

    logging challenge

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
@@ -18,6 +20,7 @@ func main() {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 	signal.Notify(ch, syscall.SIGTERM)
+	fmt.Println("server started")
 	go func() {
 		oscall := <-ch
 		log.Warn().Msgf("system call:%+v", oscall)
@@ -25,15 +28,17 @@ func main() {
 	}()
 
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", handler)
 
 	// start: set up any of your logger configuration here if necessary
 
+	logConfig()
 	// end: set up any of your logger configuration here
 
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: r,
+		Handler: logMiddleware(r),
 	}
 
 	go func() {
@@ -50,9 +55,13 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+	fmt.Println("request received")
 	name := r.URL.Query().Get("name")
 	res, err := greeting(ctx, name)
 	if err != nil {
+		log.Ctx(ctx).Error().Err(err).
+			Str("name", name).
+			Msg("greeting failed")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -60,8 +69,63 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func greeting(ctx context.Context, name string) (string, error) {
+	log.Ctx(ctx).Info().Str("name", name).Msg("greeting")
+
+	funcA(ctx)
+
+	if name == "" {
+		return "", fmt.Errorf("name is empty")
+	}
+
 	if len(name) < 5 {
 		return fmt.Sprintf("Hello %s! Your name is to short\n", name), nil
 	}
 	return fmt.Sprintf("Hi %s", name), nil
+}
+
+func logConfig() {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	lf, err := os.OpenFile("logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to open log file")
+		panic(err)
+	}
+
+	multiWriter := zerolog.MultiLevelWriter(os.Stdout, lf)
+	log.Logger = zerolog.New(multiWriter).With().Timestamp().Logger()
+	log.Info().Msg("log configured")
+
+}
+
+func logMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		log := log.With().
+			Str("request_id", uuid.New().String()).
+			Logger()
+
+		ctx := log.WithContext(r.Context())
+
+		log.Debug().
+			Ctx(ctx).
+			Str("uri", r.URL.String()).
+			Str("method", r.Method).
+			Str("IP", r.RemoteAddr).
+			Msg("request received")
+
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func funcA(ctx context.Context) {
+	log.Ctx(ctx).Info().Msg("funcA")
+	funcB(ctx)
+
+}
+
+func funcB(ctx context.Context) {
+	log.Ctx(ctx).Info().Msg("funcB")
 }
